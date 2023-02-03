b9a95bdad18f7e2b091bc8f376e34c733c9b4913
karavan0v <karavan0v02@mail.ru>
2023-02-03 06:50:48 +0300

    lab-1

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,12 +3,13 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 public class MainController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello, Spring!";
+    public String hello() {return "Hello, Karavanov Ivan Vladimirovich  " + LocalDate.now().toString();
     }
 
 }
